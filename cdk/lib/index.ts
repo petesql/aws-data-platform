eccbd7d12a61405b8a56dd169f01ef76f8492a98
pete <peter_whyte@outlook.com>
2021-11-22 15:03:59 +0000

    add iam module and new ro user

--- cdk/lib/index.ts
+++ cdk/lib/index.ts
@@ -1,10 +1,20 @@
 import * as cdk from '@aws-cdk/core';
+import * as iam from "@aws-cdk/aws-iam";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    
-    // The code that defines your stack goes here
+    const readonlyManagedPolicy = iam.ManagedPolicy.fromAwsManagedPolicyName(
+      'AmazonEC2ReadOnlyAccess',
+    );
+
+    const user = new iam.User(this, 'iamUser', {
+      userName: 'pw-test-user-1',
+      managedPolicies: [ 
+        readonlyManagedPolicy,
+      ]
+    })
+
   }
 }
