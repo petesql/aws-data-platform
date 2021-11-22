8f63b89f345721690969b3730332619aedcdd47c
pete <peter_whyte@outlook.com>
2021-11-22 14:56:55 +0000

    rename ep to index

--- cdk/lib/index.ts
+++ cdk/lib/index.ts
@@ -4,6 +4,7 @@ export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    
     // The code that defines your stack goes here
   }
 }
